1b9aaf79aafbd1df10a1a9a5a10033300ca38dcc
ykollu <ykollu@INHYNYKOLLU1.infor.com>
2019-09-19 17:21:00 +0530

    added one file

---
+++ testEX/src/example/UdemyTest.java
@@ -0,0 +1,14 @@
+package example;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UdemyTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
